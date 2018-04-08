06bb9c0f7d0b2b77a38db1daa5f619b79ab963aa
Khwanchanok Srimool <khwanchanok.sri@ascendcorp.com>
2018-04-08 15:39:29 +0700

    config api to accept request from all origin

--- src/main/java/com/service/DispenseService.java
+++ src/main/java/com/service/DispenseService.java
@@ -7,10 +7,7 @@ import com.exception.InvalidAmountException;
 import com.model.ResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by khwanchanok on 4/6/2018 AD.
@@ -22,6 +19,7 @@ public class DispenseService {
     @Autowired
     BankCalculator bankCalculator;
 
+    @CrossOrigin(origins = "*", allowCredentials = "true")
     @GetMapping("/dispense")
     public ResponseEntity<ResponseWrapper> deposit(
             @RequestParam("amount") int amount)
